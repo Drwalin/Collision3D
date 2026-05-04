0c88bbbadf6dfdd6fb725d82c166f3a7ea3523ab
Drwalin <34726800+Drwalin@users.noreply.github.com>
2026-05-04 14:26:17 +0200

    Replace .ycm_extra_conf.py with .clangd

--- .ycm_extra_conf.py
+++
@@ -1,8 +0,0 @@
-def Settings( **kwargs ):
-  return {
-    'flags': ['-x', 'c++', '-Wall', '-pedantic',
-    '-std=c++23',
-    '-I/usr/include',
-    '-ISpatialPartitioning/thirdparty/glm',
-    ],
-  }
